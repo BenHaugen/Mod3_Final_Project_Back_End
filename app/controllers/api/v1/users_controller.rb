25bf90d95d723c8b02f4d3c8709fece19ab664af
github username <github email address>
2019-03-25 14:51:42 -0700

    Can now create users on index

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,2 +1,10 @@
 class Api::V1::UsersController < ApplicationController
+
+  def index
+    @users = User.all
+   byebug
+    render json: @users
+    # puts "here are user #{@users}"
+  end
+
 end
--- db/migrate/20190325203954_create_user_events.rb
+++ db/migrate/20190325203954_create_user_events.rb
@@ -1,8 +1,8 @@
 class CreateUserEvents < ActiveRecord::Migration[5.2]
   def change
     create_table :user_events do |t|
-      t.string :user_id
-      t.string :event_id
+      t.integer :user_id
+      t.integer :event_id
 
       t.timestamps
     end
