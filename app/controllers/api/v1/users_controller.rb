99e6adda1fcb1292972fa37fe46182570d72b1f0
github username <github email address>
2019-03-29 15:15:22 -0700

    changed create action

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -11,7 +11,8 @@ class Api::V1::UsersController < ApplicationController
   end
 
   def create
-    @user = User.create(user_params)
+    @user = User.find_or_create_by(user_params)
+    render json: @user
   end
 
   private
